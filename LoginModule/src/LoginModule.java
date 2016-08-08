8ee8009cb78376d35aeb6918289cc2886944a729
Raj Negi <rajpremsinghnegi@gmail.com>
2016-08-08 14:42:33 +0530

    Inited repository

---
+++ LoginModule/src/LoginModule.java
@@ -0,0 +1,10 @@
+/**
+ * Created by webonise on 8/8/16.
+ */
+public class LoginModule {
+
+    public static void main(String[] args){
+            System.out.print("Login into the system:");
+    }
+
+}
